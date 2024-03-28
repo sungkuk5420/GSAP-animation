309d75c5e4ce9e1c2b228ed9035a567c37347c6f
Sungkuk Kim <sungkuk5420@gmail.com>
2024-03-28 11:23:40 +0900

    타임라인의 컨트롤과 라벨링

--- main.js
+++ main.js
@@ -7,7 +7,31 @@ timeline.from('.gress',{duration:1, opacity:0, y:50,stagger:{
   each:0.2,
   from:"center"
 }});
+timeline.add('test');
 timeline.from('.bird',{duration:1, opacity:0, y:50});
 timeline.from('.music',{duration:1, opacity:0, x:50,y:50},"<");
 timeline.from('.eye-left, .eye-right',{duration:1,x:30,repeat:-1,yoyo:true});
 timeline.to('.mouse',{scaleY:0,transformOrigin:'center center',repeat:-1,yoyo:true},"<");
+
+function hasClass(className){
+  return event.target.classList.contains(className)
+}
+
+
+document.addEventListener('click', function (event) {
+  let check = event.target.classList;
+
+  if (event.target.matches('button')) { event.target.focus()}
+
+
+  if(hasClass('play')){ timeline.play() }
+  
+  if(hasClass('pause')){ timeline.pause() }
+  
+  if(hasClass('reverse')){ timeline.reverse() }
+  
+  if(hasClass('restart')){ timeline.restart() }
+  
+  if(hasClass('test')){ timeline.play('test') }
+  
+})
