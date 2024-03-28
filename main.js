6baf702a2184c55188e2a9c20fd438e415b89f1b
Sungkuk Kim <sungkuk5420@gmail.com>
2024-03-28 11:42:01 +0900

    단일 메뉴 효과, 다중 메뉴 효과, gsap.default

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
 import './style.css'
 
-let timeline = gsap.timeline();
-timeline.from('.sun',{duration:3, opacity:0, x:50,y:50});
-timeline.from('.land',{duration:1, opacity:0, x:50,y:50},"-=1.5");
-timeline.from('.gress',{duration:1, opacity:0, y:50,stagger:{
-  each:0.2,
-  from:"center"
-}});
-timeline.add('test');
-timeline.from('.bird',{duration:1, opacity:0, y:50});
-timeline.from('.music',{duration:1, opacity:0, x:50,y:50},"<");
-timeline.from('.eye-left, .eye-right',{duration:1,x:30,repeat:-1,yoyo:true});
-timeline.to('.mouse',{scaleY:0,transformOrigin:'center center',repeat:-1,yoyo:true},"<");
+const items =document.querySelectorAll('.item');
 
-function hasClass(className){
-  return event.target.classList.contains(className)
-}
+gsap.defaults({
+  duration:0.2
+})
 
-
-document.addEventListener('click', function (event) {
-  let check = event.target.classList;
-
-  if (event.target.matches('button')) { event.target.focus()}
-
-
-  if(hasClass('play')){ timeline.play() }
+items.forEach((currentItem,index)=>{
+  let tl= gsap.timeline({paused:true});
   
-  if(hasClass('pause')){ timeline.pause() }
-  
-  if(hasClass('reverse')){ timeline.reverse() }
-  
-  if(hasClass('restart')){ timeline.restart() }
-  
-  if(hasClass('test')){ timeline.play('test') }
-  
+  tl.to(currentItem.querySelector('.dash'),{x:-5,opacity:1,backgroundColor:'yellow'});
+  tl.to(currentItem.querySelector('.text'),{x:5,color:'#fff'},"<");
+  currentItem.addEventListener("mouseenter",()=>{
+    tl.play();
+  })
+  currentItem.addEventListener("mouseleave",()=>{
+    tl.reverse();
+  })
 })
